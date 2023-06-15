f754f3f30e191f274ab01fd40c41702160c6bcbd
Pranai Rao <86205652+pranai-rao@users.noreply.github.com>
2023-06-15 15:22:02 +0530

    Added testimonials, 'Quotes from the Masters', section below About Us and included highlights/links in the text to match quotes.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// noinspection ES6ConvertVarToLetConst
+
 var header = document.querySelector("header");
 var hamburger = document.querySelector(".hamburger");
 var navMenu = document.querySelector(".nav-menu");
@@ -42,4 +44,34 @@ function heroSizer() {
     else if (window.innerWidth >= 1250) {
         banner.classList.remove("active");
     }
+}
+
+// Slideshow js
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
