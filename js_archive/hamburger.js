a4786404495690d5198eb9ad4adb525be85ec104
Pranai Rao <pranai@rao.me>
2023-06-23 17:58:53 +0530

    Deleting unnecessary files

--- js_archive/hamburger.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const header = document.querySelector(".header");
-
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-    header.classList.toggle("active");
-})
