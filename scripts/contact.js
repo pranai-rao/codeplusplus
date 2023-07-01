1ed0031c71ef00950fb9e698424b76a5206a825b
Pranai Rao <pranai@rao.me>
2023-07-01 20:41:28 +0530

    Converted all backend to cpp.dev

--- scripts/contact.js
+++ scripts/contact.js
@@ -20,9 +20,9 @@ function messageSizer() {
 function sendEmail() {
     var subject = subjectTemplate.concat(ticketGenerator());
     Email.send({
-        SecureToken : "8d8e7f20-ba99-4a00-8861-ce7af724f1e4", // The secure token of the registered smtpJS email
-        To : 'pranai@rao.me', // The email that the ticket will be sent to (e.g., contact@codeplusplus.dev)
-        From : "raospalace@gmail.com", // Should be a registered smtpJS email (e.g., noreply@codeplusplus.dev)
+        SecureToken : "2dae4e95-32e6-4faa-b1a4-7023e00e193c", // The secure token of the registered smtpJS email
+        To : ['pranai.rao@codeplusplus.dev', 'maitreyi.rayasam@codeplusplus.dev'], // The email that the ticket will be sent to (e.g., contact@codeplusplus.dev)
+        From : "noreply@codeplusplus.dev", // Should be a registered smtpJS email (e.g., noreply@codeplusplus.dev)
         Subject :  subject, // Will have a(n) (ideally unique) random ticket number appended to the subjectTemplate
         Body : "Name: " + fullname.value
             + "<br> Email: " + email.value
@@ -31,18 +31,18 @@ function sendEmail() {
     }).then(
         message => alert(message + "\n" + subject));
     Email.send({
-        SecureToken : "8d8e7f20-ba99-4a00-8861-ce7af724f1e4", // The secure token of the registered smtpJS email
+        SecureToken : "2dae4e95-32e6-4faa-b1a4-7023e00e193c", // The secure token of the registered smtpJS email
         To : email.value, // The email that the ticket will be sent to (e.g., contact@codeplusplus.dev)
-        Bcc: ['pranai@rao.me', 'rayasam.maitreyi@gmail.com'], // The email of the employees/customer service team (same as 'To' field in first email)
-        From : "raospalace@gmail.com", // Should be a registered smtpJS email (e.g., noreply@codeplusplus.dev)
+        Bcc: ['pranai.rao@codeplusplus.dev', 'maitreyi.rayasam@codeplusplus.dev'], // The email of the employees/customer service team (same as 'To' field in first email)
+        From : "noreply@codeplusplus.dev", // Should be a registered smtpJS email (e.g., noreply@codeplusplus.dev)
         Subject :  subject, // Will have a(n) (ideally unique) random ticket number appended to the subjectTemplate
         Body : "Greetings from Code++!<br><br>" +
             "We wanted to let you know that we received your message and that our " +
             "customer service team will be getting back to you as soon as possible. If this is a question for your teacher " +
-            "in an ongoing class, contact them directly for the fastest response. If this is an urgent request, please" +
+            "in an ongoing class, contact them directly for the fastest response. If this is an urgent request, please " +
             "note that we typically respond to requests within 1-2 business days.<br><br>" +
             "Here is the information we have for you. If any contact information is incorrect, please resubmit " +
-            "the contact form immediately with the correct information. Additionally, please remember" +
+            "the contact form immediately with the correct information. Additionally, please remember " +
             "to add the incorrect submission's ticket number, so we can get back to you as soon as possible.<br><br>" +
             "Name: " + fullname.value
             + "<br> Email: " + email.value
